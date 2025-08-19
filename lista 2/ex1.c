0d13b869d22344b6cdbb313e7c8329cfa3c6ff50
Gabee <gabe.limtee@gmail.com>
2025-08-19 11:37:41 -0300

    Create ex1.c

---
+++ lista 2/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    float n1,n2,n3,s1,s2,s3,soma;
+
+    printf("Insira as 3 notas: \n");
+    scanf("%f",n1);
+    scanf("%f",n2);
+    scanf("%f",n3);
+
+    s1 = n1 * 2;
+    s2 = n2 * 3;
+    s3 = n3 * 5;
+    soma = s1 + s2 + s3 / 10;
+
+    if (soma > 8){
+        
+    }
+
+}
